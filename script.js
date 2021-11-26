bfb1e659355893b2168654d6402d38c12a5da9e9
JideOgun <JideOgun@github.com>
2021-11-26 11:47:27 -0600

    started over - timer working correctly

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
+var body = document.body;
+let timeLeft  = 100;
 var timerEl = document.getElementById('timertext');
 
+var timerCountdown = function(){
+    let timerInterval = setInterval(function(){timeLeft--;
+    timerEl.textContent = timeLeft;
+if(timeLeft <=0) {
+    clearInterval(timerInterval)
+}}, 1000)
+};
 
+let startButtonEl = document.getElementById("start-btn");
+startButtonEl.addEventListener("click", timerCountdown)
 
-var startButEl = document.getElementById("start-btn");
-let count = 100;
-startButEl.addEventListener("click",function(){setInterval(function(){
-    
-    count--;
-    timerEl.textContent = count;
-}, 1000)
-
-});
